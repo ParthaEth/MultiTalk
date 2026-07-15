14aeb6abf30c27629bda6b2875b7daf04d811eaa
Partha Ghosh <parthaghosh.iitbbs@gmal.com>
2026-07-15 16:35:20 +0200

    Picking up the samples per setp from env var

--- config.py
+++ config.py
@@ -1,10 +1,11 @@
+import os
+
 AVATAR_DIR = '/home/web/partha/vidLink/video_generators/multitalk/Input_outputs/input_files/sales_executive'
 CKPT_DIR = './weights/Wan2.1-I2V-14B-480P'
 WAV2VEC_DIR = './weights/chinese-wav2vec2-base'
 KOKORO_DIR = './weights/Kokoro-82M'
 TTS_VOICE = './weights/Kokoro-82M/voices/af_heart.pt'
-SAMPLE_STEPS = 30
-
+SAMPLE_STEPS = int(os.getenv("MULTI_TALK_SAMPLE_STEPS", "30"))
 # Time required to generate 1 second of video (in seconds).
 # For multitalk: 4 minutes per video second = 240 seconds
 TIME_PER_VIDEO_SECOND_SECONDS = 300.0
